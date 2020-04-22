adbd0de452b510c72e3046d85889b02c00432508
julia-denivet <julia.denivet@laplateforme.io>
2020-04-22 21:04:18 +0200

    Paga index fini

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Acme&family=Amatic+SC:wght@700&family=Yanone+Kaffeesatz:wght@500&display=swap" rel="stylesheet">
+        <title>Accueil</title>
+    </head>
+
+    <body id="body_index">
+     
+            <?php include ('header.php'); ?>
+    
+	    <section id="section_index">
+
+            <?php
+                $connexion = mysqli_connect('localhost','root','','rush');
+                $sql = "SELECT login FROM utilisateurs WHERE id = '".$_SESSION['id']."'";
+                $query = mysqli_query($connexion,$sql);
+
+                if(isset($_SESSION['login']))
+                {
+                echo "<h1 id='h1_acc'>Salut, ".$_SESSION['login']." ! Tu peux maintenant acceder au fil de discussion !</h1>";
+                
+                }
+
+                else {
+                echo "<h1 id='h1_acc'>Tu cherches désespérément à vaincre l'ennuie pendant ce confinement ? Tu es au bon endroit !</h1>";
+                }
+
+            ?>
+
+		
+		
+	</section>
+</body>
+</html>
